5d8c5de3f34392772d22f41957d3319bbcc4647e
Kop4444 <144805490+Kop4444@users.noreply.github.com>
2023-12-04 12:08:38 +0300

    Add files via upload

---
+++ ДКР№2_зд1.pas
@@ -0,0 +1,27 @@
+﻿5
+Program pr;
+var 
+i,c:integer;
+a,n:array [1..10] of integer;
+
+begin
+  c:=1;
+  for i:= 1 to 10 do
+  readln(a[i]);
+  for i:= 1 to 10 do
+  n[i]:=-45786;
+  for i:=1 to 10 do begin
+    if a[i] not in n then begin
+      n[c]:=a[i];
+      c:= c + 1;
+    end;
+  end;
+  c:=1;
+  i:=0;
+  while (n[c] <> -45786) and (c<=10) do begin
+    i:= i + 1;
+    c:= c + 1;
+  end;
+  writeln();
+  writeln('Кол-во неповторяющихся элементов в массиве: ',i);
+end.
---
+++ ДКР№2_зд2.pas
@@ -0,0 +1,34 @@
+﻿Program pr;
+var 
+i,c,m,v,ct,t,dlin,ctn:integer;
+a,b:string;
+
+begin
+ct:=0;
+ctn:=1;
+v:=1;
+t:=0;
+readln(a);
+readln(b);
+dlin:=Length(a);
+m:=Length(b);
+for i:=1 to dlin do begin
+  c:=i;
+  if t > 0 then
+    t:= t-1;
+  while (a[c] = b[v]) and (v < m) and (c < dlin) do begin
+    c:=c+1;
+    v:=v+1;
+    if v = m then
+      ct:=ct+1
+    end;
+  v:=1;
+  if ct=2 then begin
+    t:=m+1;
+    ct:=0;
+    end;
+  if t = 0 then begin
+    write(a[i]);
+    end;
+  end;
+end.
